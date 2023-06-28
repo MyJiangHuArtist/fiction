b578f0796ef8613f8d481b8d25172b8a8367fa28
The Jianghuartist <jianghuartist@yeah.net>
2023-06-29 00:14:04 +0800

    init

---
+++ server/src/main/java/com/artist/FictionApplication.java
@@ -0,0 +1,13 @@
+package com.artist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FictionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FictionApplication.class, args);
+	}
+
+}
