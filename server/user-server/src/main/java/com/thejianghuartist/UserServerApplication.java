b7c09236381528ebb6351a10f2364a6a2c7387a9
The Jianghuartist <jianghuartist@yeah.net>
2023-07-10 01:05:25 +0800

    init project

---
+++ server/user-server/src/main/java/com/thejianghuartist/UserServerApplication.java
@@ -0,0 +1,12 @@
+package com.thejianghuartist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServerApplication {
+    public static void main(String[] args) {
+        //启动springApplication
+        SpringApplication.run(UserServerApplication.class, args);
+    }
+}
